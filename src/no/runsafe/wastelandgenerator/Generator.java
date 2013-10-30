2e6a5391d5d5db02e576d8235119ec2e2e3eb974
Kruithne <kruithne@gmail.com>
2013-10-30 01:58:31 +0000

    Implement the generator.

---
+++ src/no/runsafe/wastelandgenerator/Generator.java
@@ -0,0 +1,39 @@
+package no.runsafe.wastelandgenerator;
+
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.generator.ChunkGenerator;
+
+import java.util.Random;
+
+public class Generator extends ChunkGenerator
+{
+	@Override
+	public byte[][] generateBlockSections(World world, Random random, int cx, int cz, BiomeGrid biomes)
+	{
+		byte[][] result = new byte[world.getMaxHeight() / 16][];
+
+		for (int x = 0; x < 16; x++)
+		{
+			for (int z = 0; z < 16; z++)
+			{
+				result = setBlock(result, x, 60, z, (byte) Material.DIRT.getId());
+				if (random.nextInt(4) == 2)
+					result = setBlock(result, x, 61, z, (byte) Material.LONG_GRASS.getId());
+			}
+		}
+
+		return result;
+	}
+
+	private byte[][] setBlock(byte[][] result, int x, int y, int z, byte block)
+	{
+		if (result[y >>4] == null)
+			result[y >> 4] = new byte[4096];
+
+		result[y >> 4][((y & 0xF) << 8) | (z << 4) | x] = block;
+		return result;
+	}
+
+	private Random random = new Random();
+}
--- src/no/runsafe/wastelandgenerator/Plugin.java
+++ src/no/runsafe/wastelandgenerator/Plugin.java
@@ -1,12 +1,24 @@
 package no.runsafe.wastelandgenerator;
 
 import no.runsafe.framework.RunsafePlugin;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.generator.ChunkGenerator;
+
+import java.util.Random;
 
 public class Plugin extends RunsafePlugin
 {
 	@Override
-	protected void PluginSetup()
+	protected void PluginSetup() {}
+
+	public ChunkGenerator getDefaultWorldGenerator(String worldName, String id)
 	{
-		//addComponent(SomeComponent.class);
+		return new Generator();
+	}
+
+	public Location getFixedSpawnLocation(World world, Random random)
+	{
+		return new Location(world, 0.5D, 64D, 0.5D);
 	}
 }
